5903d33f0fb268e66a256234b9dacb2ebc2a1905
Abdelrahman Sadat <abdelrahmansadat1100001@gmail.com>
2020-07-13 07:40:44 +0200

    - fix: added a default value in company model

--- Task/app/Company.php
+++ Task/app/Company.php
@@ -10,4 +10,13 @@ class Company extends Model
     public function users(){
         return $this->hasMany('App\User');
     }
+    
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'logo' => 'logo.png',
+    ];
 }
