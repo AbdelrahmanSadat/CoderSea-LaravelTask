61ad81b50233d29349a7e2f6f33de86dd92f4ea4
Abdelrahman Sadat <abdelrahmansadat1100001@gmail.com>
2020-07-13 10:36:36 +0200

    - fix: escaping spaces in vars passed to blade - didn't have quotations around {{}} when passing them to attributes - that leads to issues in text after the first space if the {{}} var is string

--- Task/resources/views/company/form.blade.php
+++ Task/resources/views/company/form.blade.php
@@ -10,17 +10,17 @@
 </head>
 
 <body>
-    <form method='POST' action={{$action}}>
+    <form method='POST' action="{{$action}}">
         @method($method??'POST')
         <div class="container">
             @csrf
             <div class="form-group">
                 <label for="name">Name</label>
-                <input type="text" name="name" class="form-control" id="name" value={{$name??''}}>
+                <input type="text" name="name" class="form-control" id="name" value="{{$name??''}}">
             </div>
             <div class="form-group">
                 <label for="name">Email</label>
-                <input type="name" name="email" class="form-control" id="name" value={{$email??''}}>
+                <input type="email" name="email" class="form-control" id="name" value="{{$email??''}}">
             </div>
             <!-- TODO: LOGO -->
             <button type="submit" class="btn btn-primary">Submit</button>
